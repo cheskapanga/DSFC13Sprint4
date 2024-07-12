f369a7e88be73f0b7900482f6a00ebf59187d3d3
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-07-12 21:01:29 +0900

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -7,8 +7,9 @@ def main():
     # Define the pages
     pages = {
         "English": {
-            "title": "Title",
+            "title": "PAMiLYA - Philippine Family Law Chatbot",
             "about": "About the App",
+            "Empowering Access to Legal Help:": "In the Philippines, many underprivileged families face legal issues but cannot readily  afford the high costs associated with legal assistance. PAMiLYA is designed to bridge this gap, offering free and accessible pre-legal consultation related to family law. This chatbot aims to empower families and communities by providing clear, understandable guidance on their rights and legal options."
             "how_to_use": """
             Insert your instructions or guide on how to use the app here.
             You can use Markdown for formatting:
@@ -17,7 +18,7 @@ def main():
             """
         },
         "Filipino": {
-            "title": "Pamagat",
+            "title": "PAMiLYA - Philippine Family Law Chatbot",
             "about": "Tungkol sa App",
             "how_to_use": """
             Ilagay ang mga tagubilin o gabay kung paano gamitin ang app dito.
@@ -29,7 +30,7 @@ def main():
     }
 
     # Sidebar to select the page
-    page_selection = st.sidebar.radio("Select Page", list(pages.keys()), index=list(pages.keys()).index(page))
+    page_selection = st.sidebar.radio("Home Page", list(pages.keys()), index=list(pages.keys()).index(page))
     st.experimental_set_query_params(page=page_selection)
 
     # Display the selected page content
