564a1058fd1de9313019830d6b079f81719428fc
cheskapanga <148071219+cheskapanga@users.noreply.github.com>
2024-07-13 03:17:49 +0900

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -10,12 +10,14 @@ def main():
             "title": "PAMiLYA: Philippine Family Law Chatbot - Empowering Access to Legal Help",
             "about": "About the App",
             "description": "In the Philippines, many underprivileged families face legal issues but cannot readily afford the high costs associated with legal assistance. PAMiLYA, a database of family relations related legal cases and resolutions, aims to bridge this gap. It offers free and accessible pre-legal consultation related to family law, aiding families, students, lawmakers, law enforcers, societal organizations, and law enthusiasts in gaining awareness and understanding of Philippine Family Law. This initiative also aims to assist Barangay officials or LGU units in providing necessary legal counsel to resolve family conflicts or disputes. PAMiLYA seeks to empower families and communities by providing clear, understandable guidance on their rights and legal options.",
+            "how_to": "How to Use the App",
             "how_to_use": """
             Insert your instructions or guide on how to use the app here.
             You can use Markdown for formatting:
             - Bullet 1
             - Bullet 2
             """,
+            "limitations_header": "Limitasyon ng App",
             "limitations": "sample limitations", "st.markdown": "---",
             "citations": """
             1. UST Faculty. (2021). Persons and Family Relations. Retrieved from https://www.ustcivillaw.com/wp-content/uploads/2021/08/Persons-and-Family-Relations_HVNj62YQuGoFTpNVUVNb.pdf
@@ -25,12 +27,14 @@ def main():
             "title": "PAMiLYA: Chatbot para sa Philippine Family Law - Pinapaigtingin Makamit ang Tulong Legal",
             "about": "Tungkol sa App",
             "description": "Sa Pilipinas, maraming pamilyang mahihirap ang hinaharap ang mga isyung legal ngunit hindi agad makakaya ang mataas na gastos na kaakibat ng tulong legal. Ang PAMiLYA, isang database ng mga kaso at resolusyon sa batas pamilya, ay nilikha upang punan ang agwat na ito. Nag-aalok ito ng libre at abot-kayang konsultasyon bago mag-legal na nauugnay sa batas pamilya, tumutulong sa mga pamilya, mag-aaral, mga mambabatas, mga tagapagtupad ng batas, mga organisasyon sa lipunan, at mga tagahanga ng batas na maunawaan ang batas pamilya sa Pilipinas. Layunin din nito ang pagtulong sa mga opisyal ng Barangay o mga yunit ng LGU sa pagbibigay ng kaukulang payo legal upang lutasin ang mga alitan o labanang pamilya. Ang PAMiLYA ay layuning palakasin ang mga pamilya at komunidad sa pamamagitan ng malinaw at maaunawaang patnubay tungkol sa kanilang mga karapatan at legal na mga pagpipilian.",
+            "how_to": "Paano Gamitin and App",
             "how_to_use": """
             Ilagay ang mga tagubilin o gabay kung paano gamitin ang app dito.
             Pwede kang gumamit ng Markdown para sa pag-format:
             - Tuldok 1
             - Tuldok 2
             """,
+            "limitations_header": "Limitasyon ng App",
             "limitations": "sample limitations", "st.markdown": "---",
             "citations": """
             1. UST Faculty. (2021). Persons and Family Relations. Retrieved from https://www.ustcivillaw.com/wp-content/uploads/2021/08/Persons-and-Family-Relations_HVNj62YQuGoFTpNVUVNb.pdf
@@ -46,9 +50,9 @@ def main():
     st.title(pages[page_selection]["title"])
     st.header(pages[page_selection]["about"])
     st.write(pages[page_selection]["description"])
-    st.header('How to Use the App')
+    st.header(pages[page_selection]["how_to"])
     st.write(pages[page_selection]["how_to_use"])
-    st.header('Limitations of the App')
+    st.header(pages[page_selection]["limitations_header"])
     st.write(pages[page_selection]["limitations"])
     st.markdown("---")
     st.write('Citations')
